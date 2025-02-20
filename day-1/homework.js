d60aba66d238dbf6d7b1c90c6e689c9cc50cc09b
Miran Bae <miranbae@Mirans-MacBook-Air-2.local>
2025-02-20 18:14:43 -0500

    add more consloes

---
+++ day-1/homework.js
@@ -0,0 +1,7 @@
+console.log("good morning, eveyone!.")
+console.log("miran bae")
+console.log("pasta", "chicken", "bread")
+console.log("06301986")
+console.log("38")
+console.log("My name is miran and I am 38 years old.")
+console.log("The current year is 2025. The next year will be 2026.")
